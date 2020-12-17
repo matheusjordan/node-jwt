85a37f9915c79a7b2066ae3683e02a13b2c96b85
= <=>
2020-12-16 23:21:06 -0300

    Adicionado endpoint de autenticaçao com geraçao de token

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// bibliotecas do jwt
+require("dotenv-safe").config({example: ".env"});
+const jwt = require('jsonwebtoken');
+
 //index.js
 const http = require('http'); 
 const express = require('express'); 
@@ -13,7 +17,22 @@ app.get('/', (req, res, next) => {
 app.get('/clientes', (req, res, next) => { 
     console.log("Retornou todos clientes!");
     res.json([{id:1,nome:'luiz'}]);
-}) 
+})
+
+// autenticação
+app.post('/login', (req, res, next) => {
+    //esse teste abaixo deve ser feito no seu banco de dados
+    if(req.body.user === 'teste' && req.body.password === 'teste'){
+      //auth ok
+      const id = 1; //esse id viria do banco de dados
+      const token = jwt.sign({ id }, process.env.SECRET, {
+        expiresIn: 300 // expires in 5min
+      });
+      return res.json({ auth: true, token: token });
+    }
+    
+    res.status(500).json({message: 'Login inválido!'});
+})
 
 const server = http.createServer(app); 
 server.listen(3000);
